0e32a791fafa054d2e7fafad1660620c54de73e2
alluriprasad <alluriprasad@HDDLBGKIOSK13.Virtusa.com>
2019-06-13 10:43:11 +0530

    commited

---
+++ src/main/java/test/lab/sample.java
@@ -0,0 +1,14 @@
+package test.lab;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a,b,c;
+a=10;
+b=12;
+c=a+b;
+System.out.println(c);
+	}
+
+}
